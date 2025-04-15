13f26f15ec30e7b25409d47c786c2ec45db8cc67
ramakrishnanyadav <119944138+ramakrishnanyadav@users.noreply.github.com>
2025-04-15 18:16:49 +0530

    Add files via upload

---
+++ FacultyApp/FacultyApp/settings.gradle.kts
@@ -0,0 +1,19 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+        maven { url = uri("https://jitpack.io") }
+        maven { url =uri("https://storage.zego.im/maven")}
+    }
+}
+
+rootProject.name = "FacultyApp"
+include(":app")
